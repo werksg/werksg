4b3ec7bf094a9754ec0fa0904b872da5a142cc0a
Aaron Lim CK <aaronlimckdeveloper@gmail.com>
2023-02-16 21:51:29 +0800

    initial dist subtree commit

--- src/components/Footer.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <footer class="flex items-center justify-center" style="height: 60px">
-    <p class="text-gray-400 text-xs text-center">
-      © 2023 Werk Pte. Ltd. (UEN: 202242757D). <br />
-      All rights reserved.
-    </p>
-  </footer>
-</template>
