f127933f746efce01ea971a872d375cfeb06a245
Aaron Lim <aaronlimchengkiat@gmail.com>
2023-06-09 14:55:43 +0800

    Update Footer.vue
    Updated footer from Werk Pte. Ltd. to WerkSG Pte. Ltd.

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="flex items-center justify-center" style="height: 60px">
     <p class="text-gray-400 text-xs text-center">
-      © 2023 Werk Pte. Ltd. (UEN: 202242757D). <br />
+      © 2023 WerkSG Pte. Ltd. (UEN: 202242757D). <br />
       All rights reserved.
     </p>
   </footer>
